23dff42145bddb9315395d08f86a6c84e98dfa91
Scorpio-BS <sbubrak.f@gmail.com>
2021-06-20 23:29:02 +0500

    Text Added to Buttons

--- Tic Tac Toe Win Using C++/TicTacToe.cpp
+++ Tic Tac Toe Win Using C++/TicTacToe.cpp
@@ -4,6 +4,7 @@
 #pragma hdrstop
 
 #include "TicTacToe.h"
+
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.fmx"
@@ -14,3 +15,86 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
+
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+		Button1->Text = "X";
+}
+//---------------------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+    Button2->Text = "X";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button3Click(TObject *Sender)
+{
+    Button3->Text = "X";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button4Click(TObject *Sender)
+{
+	Button4->Text = "X";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button5Click(TObject *Sender)
+{
+	Button5->Text = "X";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button6Click(TObject *Sender)
+{
+	Button6->Text = "X";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button7Click(TObject *Sender)
+{
+	Button7->Text = "X";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button8Click(TObject *Sender)
+{
+	Button8->Text = "X";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button9Click(TObject *Sender)
+{
+	Button9->Text = "X";
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button10Click(TObject *Sender)
+{
+	Button1->Text = "1";
+	Button2->Text = "2";
+	Button3->Text = "3";
+	Button4->Text = "4";
+	Button5->Text = "5";
+	Button6->Text = "6";
+	Button7->Text = "7";
+	Button8->Text = "8";
+	Button9->Text = "9";
+
+
+
+
+
+
+
+
+
+}
+//---------------------------------------------------------------------------
+
